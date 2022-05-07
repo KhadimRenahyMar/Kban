52b5cea91e96b3baf73fb6cf245f2c72544040d9
KhadimRenahyMar <dovahkrm@gmail.com>
2022-05-07 15:27:34 +0200

    heroku setup

--- app/database.ts
+++ app/database.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 import { Sequelize } from 'sequelize';
 console.log(process.env.PG_URL);
-const client = new Sequelize(process.env.DATABASE_URL, {
+const client = new Sequelize(process.env.PG_URL, {
     define: {
         timestamps: false,
         createdAt: "createdAt",
