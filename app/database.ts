7c43ec293ff11b258851090d61cf90b5ee60d803
KhadimRenahyMar <dovahkrm@gmail.com>
2022-05-07 16:09:22 +0200

    heroku setup

--- app/database.ts
+++ app/database.ts
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
 import { Sequelize } from 'sequelize';
+const url = window.location.origin;
+console.log(url);
 console.log(process.env.PG_URL);
 console.log(process.env.DATABASE_URL);
-const client = new Sequelize(process.env.PG_URL, {
+const client = new Sequelize(process.env.PG_URL || process.env.HEROKU_POSTGRESQL_PUCE_URL, {
     define: {
         timestamps: false,
         createdAt: "createdAt",
