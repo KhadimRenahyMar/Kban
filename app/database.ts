d90fb672f231256d0e6eafdc58c5605f3b7007a8
ZIMA <dovahkrm@gmail.com>
2022-09-16 09:37:29 +0200

    chore: deployment

--- app/database.ts
+++ app/database.ts
@@ -1,14 +1,17 @@
 require('dotenv').config();
 
 import { Sequelize } from 'sequelize';
-console.log("pgURL", process.env.PG_URL);
 let URL = process.env.NODE_ENV === "production" ? process.env.HEROKU_POSTGRESQL_PUCE_URL : process.env.PG_URL;
-console.log('url', URL)
-const client = new Sequelize(process.env.PG_URL || process.env.HEROKU_POSTGRESQL_PUCE_URL, {
+const client = new Sequelize(URL, {
     define: {
         timestamps: false,
         createdAt: "createdAt",
         updatedAt: "updatedAt",
+    },
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
